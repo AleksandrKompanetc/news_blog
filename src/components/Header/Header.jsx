79ca53d270ecf4bd15c7e35c88f7c161df760523
AleksandrKompanetc <133540503+AleksandrKompanetc@users.noreply.github.com>
2023-11-26 01:13:22 +0200

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+const Header = () => {
+    return <header className={styles.header}>
+        <h1 className={styles.title}>NEWS BLOG</h1>
+    </header>
+}
+
+export default Header;
---
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -0,0 +1,12 @@
+import Image from '../Image/Image';
+import styles from './styles.module.css';
+
+const NewsBanner = ({item}) => {
+    return <header className={styles.banner}>
+        <Image image={item?.image} />
+        {/* <h3 className={styles.title}>{item.title}</h3>
+        <p className={styles.extra}>{formatTimeAgo(item.published)} by {item.author} </p> */}
+    </header>
+}
+
+export default NewsBanner;
